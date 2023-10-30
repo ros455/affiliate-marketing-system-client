be89b36177c2310493811b70dd167a658cc3715a
ros455 <ros.kichuk@gmail.com>
2023-10-31 01:20:18 +0200

    added pagination and server search 31.10 -ros

--- src/components/Admin/AdvancedStatistics.jsx
+++ src/components/Admin/AdvancedStatistics.jsx
@@ -1,9 +1,106 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 const AdvancedStatistics = () => {
+    const [statistic] = useState([
+        {
+                clicks: [
+                    {
+                        date: '22.10.2023'
+                    },
+                    {
+                        date: '22.10.2023'
+                    },
+                    {
+                        date: '22.10.2023'
+                    },
+                    {
+                        date: '22.10.2023'
+                    },
+                    {
+                        date: '22.10.2023'
+                    },
+                    {
+                        date: '22.10.2023'
+                    },
+                    {
+                        date: '22.10.2023'
+                    },
+                    {
+                        date: '22.10.2023'
+                    },
+                    {
+                        date: '22.10.2023'
+                    },
+                    {
+                        date: '22.10.2023'
+                    },
+                ],
+                buys: [
+                    {
+                        date: '22.10.2023'
+                    },
+                    {
+                        date: '22.10.2023'
+                    },
+                ]
+            },
+        {
+                clicks: [
+                    {
+                        date: '22.10.2023'
+                    },
+                    {
+                        date: '22.10.2023'
+                    },
+                    {
+                        date: '22.10.2023'
+                    },
+                    {
+                        date: '22.10.2023'
+                    },
+                    {
+                        date: '22.10.2023'
+                    },
+                    {
+                        date: '22.10.2023'
+                    },
+                    {
+                        date: '22.10.2023'
+                    },
+                    {
+                        date: '22.10.2023'
+                    },
+                ],
+                buys: [
+                    {
+                        date: '22.10.2023'
+                    },
+                ]
+            },
+    ])
+    const [clicks, setClicks] = useState(0);
+    const [buys, setBuys] = useState(0);
+    const [conversion, setConversion] = useState(0);
+
+    useEffect(() => {
+        statistic.forEach((parent) => {
+            setClicks((state) => state += parent.clicks.length);
+            setBuys((state) => state += parent.buys.length);
+        })
+    },[])
+
+    useEffect(() => {
+        if(clicks && buys) {
+            setConversion((buys / clicks) * 100)
+        }
+    },[clicks, buys])
+
     return (
         <div>
             AdvancedStatistics
+            <p>Clicks {clicks}</p>
+            <p>Buys {buys}</p>
+            <p>Conversion {conversion.toFixed(1)}%</p>
         </div>
     );
 };
--- src/components/template/ListOfPartner_List.jsx
+++ src/components/template/ListOfPartner_List.jsx
@@ -1,18 +1,61 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux/es/hooks/useSelector';
-import {BsThreeDots} from 'react-icons/bs';
-import { allUsers } from '../../store/auth';
+import {AiOutlineLeft, AiOutlineRight} from 'react-icons/ai';
+import axios from 'axios';
+import { AUTH_TOKEN } from '../../utils/Token';
+import { BASE_URL } from '../../http/BaseUrl';
 const ListOfPartner_List = () => {
     const [partnerName, setPartnerName] = useState('');
-    const allUser = useSelector(allUsers);
+    const [allUsers, setAllUsers] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [visibleCurrentPage, setVisibleCurrentPage] = useState(1);
 
-    console.log('allUser',allUser);
+    const [searchTerm, setSearchTerm] = useState('');
+    const [users, setUsers] = useState([]);
+    const [page, setPage] = useState(1);
+
+    useEffect(() => {
+        const fetchData = async () => {
+          try {
+            const response = await axios.get(`${BASE_URL}/search-users`, {
+              params: { page: currentPage, limit: 2, search: searchTerm },
+              headers: { authorization: AUTH_TOKEN }
+            });
+            if(response.data.length) {
+                setAllUsers(response.data);
+                setVisibleCurrentPage(currentPage);
+            } else {
+                const lastPage = currentPage - 1;
+                setCurrentPage(lastPage);
+            }
+          } catch (error) {
+            console.error('Error fetching data: ', error);
+          }
+        };      
+        const timeoutId = setTimeout(() => {
+          fetchData();
+        }, 500); // Debounce the API call by 500ms
+      
+        return () => {
+          clearTimeout(timeoutId);
+        };
+      }, [currentPage, searchTerm]);
+      console.log('allUsers',allUsers);
+
+
+    const handleSearchChange = (event) => {
+        setSearchTerm(event.target.value);
+        setPage(1); // Reset to the first page on search
+      };
+
     return (
         <div className='admin_panel_items derection_wraper' >
             <div className='dashboard_list_header'>
             <h3 className='dashboard_list_title'>Partner</h3>
             <div className='dashboard_input_wrap'>
-                <input placeholder='Search partner'/>
+                <input
+                 onChange={(e) => handleSearchChange(e)}
+                 placeholder='Search partner'/>
             </div>
             </div>
             <div className='derection_table_wrap'>
@@ -23,7 +66,7 @@ const ListOfPartner_List = () => {
                     <p className='colum '>Sales</p>
                 </div>
                 <div className='table_body'>
-                    {!!allUser.length && allUser.map((user) => (
+                    {!!allUsers.length && allUsers.map((user) => (
                     <div className='table_info_item' key={user._id}> 
                         <p className='colum row colum_name'>{user.name}</p>
                         <p className='colum row colum_progres'>2</p>
@@ -31,6 +74,22 @@ const ListOfPartner_List = () => {
                         <p className='colum row colum_data'>4</p>
                     </div>
                     ))}
+                </div>
+            </div>
+            <div className='dashboard_pagination_wrap'>
+                <div className='dashboard_pagination_block'>
+                    <div className='dashboard_pagination_buttons_block'>
+                        <button 
+                        disabled={currentPage == 1 ? true : false}
+                        onClick={() => setCurrentPage((state) => state -= 1)}
+                        className='dashboard_pagination_button_item'>
+                            <AiOutlineLeft/> Prev</button>
+                            <p className='pagination_current_page'>{visibleCurrentPage}</p>
+                        <button 
+                        onClick={() => setCurrentPage((state) => state += 1)}
+                        className='dashboard_pagination_button_item'>
+                            Next <AiOutlineRight/></button>
+                    </div>
                 </div>
             </div>
         </div>
