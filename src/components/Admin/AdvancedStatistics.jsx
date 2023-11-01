ccbbcee969a7c0434baf86fc036c3606ecbbbefa
Oleksii Horban <lehagorban93@gmail.com>
2023-11-01 19:59:24 +0200

    create StatisticChart and UserOne components

--- src/components/Admin/AdvancedStatistics.jsx
+++ src/components/Admin/AdvancedStatistics.jsx
@@ -1,108 +1,112 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from "react";
+import StatisticChart from "../template/StatisticChart";
 
 const AdvancedStatistics = () => {
-    const [statistic] = useState([
+  const [statistic] = useState([
+    {
+      clicks: [
         {
-                clicks: [
-                    {
-                        date: '22.10.2023'
-                    },
-                    {
-                        date: '22.10.2023'
-                    },
-                    {
-                        date: '22.10.2023'
-                    },
-                    {
-                        date: '22.10.2023'
-                    },
-                    {
-                        date: '22.10.2023'
-                    },
-                    {
-                        date: '22.10.2023'
-                    },
-                    {
-                        date: '22.10.2023'
-                    },
-                    {
-                        date: '22.10.2023'
-                    },
-                    {
-                        date: '22.10.2023'
-                    },
-                    {
-                        date: '22.10.2023'
-                    },
-                ],
-                buys: [
-                    {
-                        date: '22.10.2023'
-                    },
-                    {
-                        date: '22.10.2023'
-                    },
-                ]
-            },
+          date: "22.10.2023",
+        },
         {
-                clicks: [
-                    {
-                        date: '22.10.2023'
-                    },
-                    {
-                        date: '22.10.2023'
-                    },
-                    {
-                        date: '22.10.2023'
-                    },
-                    {
-                        date: '22.10.2023'
-                    },
-                    {
-                        date: '22.10.2023'
-                    },
-                    {
-                        date: '22.10.2023'
-                    },
-                    {
-                        date: '22.10.2023'
-                    },
-                    {
-                        date: '22.10.2023'
-                    },
-                ],
-                buys: [
-                    {
-                        date: '22.10.2023'
-                    },
-                ]
-            },
-    ])
-    const [clicks, setClicks] = useState(0);
-    const [buys, setBuys] = useState(0);
-    const [conversion, setConversion] = useState(0);
+          date: "22.10.2023",
+        },
+        {
+          date: "22.10.2023",
+        },
+        {
+          date: "22.10.2023",
+        },
+        {
+          date: "22.10.2023",
+        },
+        {
+          date: "22.10.2023",
+        },
+        {
+          date: "22.10.2023",
+        },
+        {
+          date: "22.10.2023",
+        },
+        {
+          date: "22.10.2023",
+        },
+        {
+          date: "22.10.2023",
+        },
+      ],
+      buys: [
+        {
+          date: "22.10.2023",
+        },
+        {
+          date: "22.10.2023",
+        },
+      ],
+    },
+    {
+      clicks: [
+        {
+          date: "22.10.2023",
+        },
+        {
+          date: "22.10.2023",
+        },
+        {
+          date: "22.10.2023",
+        },
+        {
+          date: "22.10.2023",
+        },
+        {
+          date: "22.10.2023",
+        },
+        {
+          date: "22.10.2023",
+        },
+        {
+          date: "22.10.2023",
+        },
+        {
+          date: "22.10.2023",
+        },
+      ],
+      buys: [
+        {
+          date: "22.10.2023",
+        },
+      ],
+    },
+  ]);
+  const [clicks, setClicks] = useState(0);
+  const [buys, setBuys] = useState(0);
+  const [conversion, setConversion] = useState(0);
 
-    useEffect(() => {
-        statistic.forEach((parent) => {
-            setClicks((state) => state += parent.clicks.length);
-            setBuys((state) => state += parent.buys.length);
-        })
-    },[])
+  useEffect(() => {
+    statistic.forEach((parent) => {
+      setClicks((state) => (state += parent.clicks.length));
+      setBuys((state) => (state += parent.buys.length));
+    });
+  }, []);
 
-    useEffect(() => {
-        if(clicks && buys) {
-            setConversion((buys / clicks) * 100)
-        }
-    },[clicks, buys])
+  useEffect(() => {
+    if (clicks && buys) {
+      setConversion((buys / clicks) * 100);
+    }
+  }, [clicks, buys]);
 
-    return (
-        <div>
-            AdvancedStatistics
-            <p>Clicks {clicks}</p>
-            <p>Buys {buys}</p>
-            <p>Conversion {conversion.toFixed(1)}%</p>
-        </div>
-    );
+  return (
+    // <div>
+    //     AdvancedStatistics
+    //     <p>Clicks {clicks}</p>
+    //     <p>Buys {buys}</p>
+    //     <p>Conversion {conversion.toFixed(1)}%</p>
+    // </div>
+    <>
+      <StatisticChart />
+    </>
+  );
 };
 
 export default AdvancedStatistics;
