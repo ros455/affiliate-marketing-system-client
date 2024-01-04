27f298f34e0f2b559dd59a57d37518736237ec1f
ros455 <ros.kichuk@gmail.com>
2024-01-04 19:31:29 +0200

    update base url

--- src/http/BaseUrl.js
+++ src/http/BaseUrl.js
@@ -1,2 +1,2 @@
 // export const BASE_URL = 'http://localhost:4444/api';
-export const BASE_URL = 'http://affiliate.makenude.ai:4444/api';
+export const BASE_URL = 'https://api.affiliate.makenude.ai/api';
